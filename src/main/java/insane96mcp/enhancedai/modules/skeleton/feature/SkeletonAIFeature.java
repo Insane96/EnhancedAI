27f605076e207f43ab2d7e52f2e39df485f2a43f
Alberto Del Villano <delvillano.alberto@gmail.com>
2021-11-28 08:51:45 +0100

    Skeletons flee speed is now configurable and it's no longer swapped

--- src/main/java/insane96mcp/enhancedai/modules/skeleton/feature/SkeletonAIFeature.java
+++ src/main/java/insane96mcp/enhancedai/modules/skeleton/feature/SkeletonAIFeature.java
@@ -25,36 +25,36 @@ public class SkeletonAIFeature extends Feature {
 
 	private final ForgeConfigSpec.ConfigValue<Integer> minShootingRangeConfig;
 	private final ForgeConfigSpec.ConfigValue<Integer> maxShootingRangeConfig;
-	private final ForgeConfigSpec.ConfigValue<Double> avoidPlayerChanceConfig;
-	private final ForgeConfigSpec.ConfigValue<Double> attackWhenAvoidingChanceConfig;
 	private final ForgeConfigSpec.ConfigValue<Double> strafeChanceConfig;
 	private final ForgeConfigSpec.ConfigValue<Double> arrowInaccuracyConfig;
 	private final BlacklistConfig entityBlacklistConfig;
+	//Flee from target
+	private final ForgeConfigSpec.ConfigValue<Double> avoidPlayerChanceConfig;
+	private final ForgeConfigSpec.ConfigValue<Double> attackWhenAvoidingChanceConfig;
+	private final ForgeConfigSpec.ConfigValue<Double> fleeSpeedNearConfig;
+	private final ForgeConfigSpec.ConfigValue<Double> fleeSpeedFarConfig;
 
 	public int minShootingRange = 24;
 	public int maxShootingRange = 48;
-	public double avoidPlayerChance = 0.5d;
-	public double attackWhenAvoidingChance = 0.5d;
 	public double strafeChance = 0.5d;
 	public double arrowInaccuracy = 2;
 	public ArrayList<IdTagMatcher> entityBlacklist;
 	public boolean entityBlacklistAsWhitelist;
+	//Flee from target
+	public double avoidPlayerChance = 0.5d;
+	public double attackWhenAvoidingChance = 0.5d;
+	public double fleeSpeedNear = 1.5d;
+	public double fleeSpeedFar = 1.25d;
 
 	public SkeletonAIFeature(Module module) {
 		super(Config.builder, module);
-		Config.builder.comment(this.getDescription()).push(this.getName());
+		super.pushConfig(Config.builder);
 		minShootingRangeConfig = Config.builder
 				.comment("The min range from where a skeleton will shoot a player")
 				.defineInRange("Min Shooting Range", this.minShootingRange, 1, 64);
 		maxShootingRangeConfig = Config.builder
 				.comment("The max range from where a skeleton will shoot a player")
 				.defineInRange("Max Shooting Range", this.maxShootingRange, 1, 64);
-		avoidPlayerChanceConfig = Config.builder
-				.comment("Chance for a Skeleton to spawn with the ability to avoid the player")
-				.defineInRange("Avoid Player chance", this.avoidPlayerChance, 0d, 1d);
-		attackWhenAvoidingChanceConfig = Config.builder
-				.comment("Chance for a Skeleton to attack while running from a player")
-				.defineInRange("Attack When Avoiding Chance", this.attackWhenAvoidingChance, 0d, 1d);
 		strafeChanceConfig = Config.builder
 				.comment("Chance for a Skeleton to spawn with the ability to strafe (like vanilla)")
 				.defineInRange("Strafe chance", this.strafeChance, 0d, 1d);
@@ -62,6 +62,21 @@ public class SkeletonAIFeature extends Feature {
 				.comment("How much inaccuracy does the arrow fired by skeletons have. Vanilla skeletons have 10/6/2 inaccuracy in easy/normal/hard difficulty.")
 				.defineInRange("Arrow Inaccuracy", this.arrowInaccuracy, 0d, 30d);
 		entityBlacklistConfig = new BlacklistConfig(Config.builder, "Entity Blacklist", "Entities that shouldn't get the enhanced Shoot AI", Arrays.asList("quark:forgotten"), false);
+
+		Config.builder.push("Flee from Target");
+		avoidPlayerChanceConfig = Config.builder
+				.comment("Chance for a Skeleton to spawn with the ability to avoid the player")
+				.defineInRange("Avoid Player chance", this.avoidPlayerChance, 0d, 1d);
+		attackWhenAvoidingChanceConfig = Config.builder
+				.comment("Chance for a Skeleton to attack while running from a player")
+				.defineInRange("Attack When Avoiding Chance", this.attackWhenAvoidingChance, 0d, 1d);
+		fleeSpeedNearConfig = Config.builder
+				.comment("Speed multiplier when the skeleton avoids the player and it's within 7 blocks from him.")
+				.defineInRange("Flee speed Multiplier Near", this.fleeSpeedNear, 0d, 4d);
+		fleeSpeedFarConfig = Config.builder
+				.comment("Speed multiplier when the skeleton avoids the player and it's farther than 7 blocks from him.")
+				.defineInRange("Flee speed Multiplier Far", this.fleeSpeedFar, 0d, 4d);
+		Config.builder.pop();
 		Config.builder.pop();
 	}
 
@@ -70,12 +85,15 @@ public class SkeletonAIFeature extends Feature {
 		super.loadConfig();
 		this.minShootingRange = this.minShootingRangeConfig.get();
 		this.maxShootingRange = this.maxShootingRangeConfig.get();
-		this.avoidPlayerChance = this.avoidPlayerChanceConfig.get();
-		this.attackWhenAvoidingChance = this.attackWhenAvoidingChanceConfig.get();
 		this.strafeChance = this.strafeChanceConfig.get();
 		this.arrowInaccuracy = this.arrowInaccuracyConfig.get();
 		this.entityBlacklist = IdTagMatcher.parseStringList(this.entityBlacklistConfig.listConfig.get());
 		this.entityBlacklistAsWhitelist = this.entityBlacklistConfig.listAsWhitelistConfig.get();
+		//Flee from target
+		this.avoidPlayerChance = this.avoidPlayerChanceConfig.get();
+		this.attackWhenAvoidingChance = this.attackWhenAvoidingChanceConfig.get();
+		this.fleeSpeedNear = this.fleeSpeedNearConfig.get();
+		this.fleeSpeedFar = this.fleeSpeedFarConfig.get();
 	}
 
 	public void reassessWeaponGoal(AbstractSkeletonEntity skeleton) {
@@ -110,7 +128,7 @@ public class SkeletonAIFeature extends Feature {
 			skeleton.goalSelector.addGoal(2, rangedBowAttackGoal);
 
 			if (skeleton.level.random.nextDouble() < this.avoidPlayerChance) {
-				AIAvoidEntityGoal<PlayerEntity> avoidEntityGoal = new AIAvoidEntityGoal<>(skeleton, PlayerEntity.class, 12.0f, 1.5d, 1.25d);
+				AIAvoidEntityGoal<PlayerEntity> avoidEntityGoal = new AIAvoidEntityGoal<>(skeleton, PlayerEntity.class, 12.0f, this.fleeSpeedFar, this.fleeSpeedNear);
 				avoidEntityGoal.setAttackWhenRunning(skeleton.level.random.nextDouble() < this.attackWhenAvoidingChance);
 				skeleton.goalSelector.addGoal(1, avoidEntityGoal);
 			}
