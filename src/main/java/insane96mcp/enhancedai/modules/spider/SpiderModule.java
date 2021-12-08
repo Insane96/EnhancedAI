9bf343e68319ceeb171266790e7707a08fc16a5f
Alberto Del Villano <delvillano.alberto@gmail.com>
2021-12-08 10:35:40 +0100

    Fixed missing Misc Spider Feature

--- src/main/java/insane96mcp/enhancedai/modules/spider/SpiderModule.java
+++ src/main/java/insane96mcp/enhancedai/modules/spider/SpiderModule.java
@@ -1,5 +1,6 @@
 package insane96mcp.enhancedai.modules.spider;
 
+import insane96mcp.enhancedai.modules.spider.feature.MiscFeature;
 import insane96mcp.enhancedai.modules.spider.feature.ThrowingWebFeature;
 import insane96mcp.enhancedai.setup.Config;
 import insane96mcp.insanelib.base.Label;
@@ -9,11 +10,13 @@ import insane96mcp.insanelib.base.Module;
 public class SpiderModule extends Module {
 
 	public ThrowingWebFeature throwingWeb;
+	public MiscFeature misc;
 
 	public SpiderModule() {
 		super(Config.builder);
 		this.pushConfig(Config.builder);
 		this.throwingWeb = new ThrowingWebFeature(this);
+		this.misc = new MiscFeature(this);
 		Config.builder.pop();
 	}
 
@@ -21,5 +24,6 @@ public class SpiderModule extends Module {
 	public void loadConfig() {
 		super.loadConfig();
 		throwingWeb.loadConfig();
+		misc.loadConfig();
 	}
 }
