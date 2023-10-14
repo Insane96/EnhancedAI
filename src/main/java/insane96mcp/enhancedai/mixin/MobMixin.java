2651b9a9c2d12c984e06f8cc913957b1918b3ef1
Insane96 <delvillano.alberto@gmail.com>
2023-10-14 11:40:17 +0200

    1.12.4 - Backported 1.20.2 attack reach change

---
+++ src/main/java/insane96mcp/enhancedai/mixin/MobMixin.java
@@ -0,0 +1,20 @@
+package insane96mcp.enhancedai.mixin;
+
+import insane96mcp.enhancedai.modules.base.feature.Attacking;
+import insane96mcp.insanelib.base.Feature;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Mob.class)
+public class MobMixin {
+    @Inject(at = @At(value = "HEAD"), method = "isWithinMeleeAttackRange", cancellable = true)
+    private void changeIsWithinMeleeAttackRange(LivingEntity attacked, CallbackInfoReturnable<Boolean> cir) {
+        if (!Feature.isEnabled(Attacking.class))
+            return;
+        cir.setReturnValue(Attacking.isWithinMeleeAttackRange((Mob) (Object) this, attacked));
+    }
+}
