4568b96a2a708a16c62127a44d473bee0e270939
Insane96 <delvillano.alberto@gmail.com>
2025-06-29 16:50:57 +0200

    Revert "2.6.8 - Monsters can now forget their target Fishing min and max distance can now be changed"
    This reverts commit b42ef45c

--- src/main/java/insane96mcp/enhancedai/modules/mobs/fisher/FishingTargetGoal.java
+++ src/main/java/insane96mcp/enhancedai/modules/mobs/fisher/FishingTargetGoal.java
@@ -41,21 +41,21 @@ public class FishingTargetGoal extends Goal {
 			return false;
 
 		--this.cooldown;
-		return this.fisher.distanceToSqr(target) > FisherMobs.minDistance * FisherMobs.minDistance
+		return this.fisher.distanceToSqr(target) > 16d //4 blocks
                 && this.fisher.getSensing().hasLineOfSight(target);
     }
 
 	public boolean canContinueToUse() {
 		return this.fisher.getTarget() != null
 				&& this.fisher.getTarget().isAlive()
-				&& this.fisher.distanceToSqr(target) > FisherMobs.minDistance * FisherMobs.minDistance
+				&& this.fisher.distanceToSqr(target) > 16d //4 blocks
 				&& this.fisher.getSensing().hasLineOfSight(target);
 	}
 
 	public void start() {
 		this.target = this.fisher.getTarget();
 		double distance = this.fisher.distanceTo(this.target);
-        if (distance <= FisherMobs.maxDistance * FisherMobs.maxDistance)
+        if (distance <= 576)
             this.fisher.getNavigation().stop();
     }
 
@@ -68,9 +68,9 @@ public class FishingTargetGoal extends Goal {
 		double dirX = this.target.getX() - this.fisher.getX();
 		double dirZ = this.target.getZ() - this.fisher.getZ();
 		double distanceXZ = Math.sqrt(dirX * dirX + dirZ * dirZ);
-		if (distance > FisherMobs.maxDistance * FisherMobs.maxDistance) {
+		if (distance > 24) {
 			if (this.fisher.getNavigation().isDone()) {
-				Path path = this.fisher.getNavigation().createPath(this.target, FisherMobs.maxDistance);
+				Path path = this.fisher.getNavigation().createPath(this.target, 24);
 				if (path != null)
 					this.fisher.getNavigation().moveTo(path, 1);
 			}
--- src/main/java/insane96mcp/enhancedai/modules/mobs/targeting/EASpiderTargetGoal.java
+++ src/main/java/insane96mcp/enhancedai/modules/mobs/targeting/EASpiderTargetGoal.java
@@ -6,9 +6,8 @@ import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.monster.Spider;
 
 public class EASpiderTargetGoal<T extends LivingEntity> extends EANearestAttackableTarget<T> {
-    public EASpiderTargetGoal(Spider goalOwnerIn, Class<T> targetClassIn, boolean checkSight, boolean nearbyOnlyIn, TargetingConditions targetingConditions, int unseenMemoryTicks) {
+    public EASpiderTargetGoal(Spider goalOwnerIn, Class<T> targetClassIn, boolean checkSight, boolean nearbyOnlyIn, TargetingConditions targetingConditions) {
         super(goalOwnerIn, targetClassIn, checkSight, nearbyOnlyIn, targetingConditions);
-        this.unseenMemoryTicks = unseenMemoryTicks;
     }
 
     /**
