ad6a562ea78874714b7e276534fbc5b33f16c71e
Insane96 <delvillano.alberto@gmail.com>
2025-07-24 23:01:43 +0200

    2.6.12 - Fixed nearest attackable target not using the correct follow range

--- src/main/java/insane96mcp/enhancedai/modules/mobs/targeting/EANearestAttackableTarget.java
+++ src/main/java/insane96mcp/enhancedai/modules/mobs/targeting/EANearestAttackableTarget.java
@@ -16,21 +16,17 @@ public class EANearestAttackableTarget<T extends LivingEntity> extends ILNearest
 
     public TargetingConditions targetEntitySelectorXRay;
 
-    public EANearestAttackableTarget(Mob goalOwnerIn, Class<T> targetClassIn, boolean mustSee, boolean mustReach) {
-        super(goalOwnerIn, targetClassIn, mustSee, mustReach, null);
-        this.targetEntitySelectorXRay = this.targetEntitySelector.copy().ignoreLineOfSight();
-    }
-
     public EANearestAttackableTarget(Mob goalOwnerIn, Class<T> targetClassIn, boolean mustSee, boolean mustReach, TargetingConditions targetingConditions) {
         super(goalOwnerIn, targetClassIn, mustSee, mustReach, null);
         this.targetEntitySelector = targetingConditions;
         this.targetEntitySelectorXRay = targetingConditions.copy().ignoreLineOfSight();
     }
 
-    public EANearestAttackableTarget(Mob goalOwnerIn, Class<T> targetClassIn, IdTagMatcher idTagMatcher, boolean mustSee, boolean mustReach) {
-        this(goalOwnerIn, targetClassIn, mustSee, mustReach);
-        this.targetEntitySelector.selector(idTagMatcher::matchesEntity);
-        this.targetEntitySelectorXRay.selector(idTagMatcher::matchesEntity);
+    public EANearestAttackableTarget(Mob goalOwnerIn, Class<T> targetClassIn, boolean mustSee, boolean mustReach, TargetingConditions targetingConditions, int forgetTicks) {
+        super(goalOwnerIn, targetClassIn, mustSee, mustReach, null);
+        this.targetEntitySelector = targetingConditions;
+        this.targetEntitySelectorXRay = targetingConditions.copy().ignoreLineOfSight();
+        this.unseenMemoryTicks = forgetTicks;
     }
 
     public EANearestAttackableTarget(Mob goalOwnerIn, Class<T> targetClassIn, IdTagMatcher idTagMatcher, boolean mustSee, boolean mustReach, TargetingConditions targetingConditions) {
@@ -39,21 +35,27 @@ public class EANearestAttackableTarget<T extends LivingEntity> extends ILNearest
         this.targetEntitySelectorXRay.selector(idTagMatcher::matchesEntity);
     }
 
+    public EANearestAttackableTarget(Mob goalOwnerIn, Class<T> targetClassIn, IdTagMatcher idTagMatcher, boolean mustSee, boolean mustReach, TargetingConditions targetingConditions, int forgetTicks) {
+        this(goalOwnerIn, targetClassIn, mustSee, mustReach, targetingConditions);
+        this.targetEntitySelector.selector(idTagMatcher::matchesEntity);
+        this.targetEntitySelectorXRay.selector(idTagMatcher::matchesEntity);
+        this.unseenMemoryTicks = forgetTicks;
+    }
+
     @Override
     protected void findTarget() {
+        this.targetEntitySelector.range(this.getFollowDistance());
+        super.findTarget();
+        if (this.nearestTarget != null
+                || this.getFollowXRayDistance() <= 0d)
+            return;
+        this.targetEntitySelectorXRay.range(this.getFollowXRayDistance());
         if (this.targetClass != Player.class && this.targetClass != ServerPlayer.class) {
-            //Try to find the nearest target without xray, then try with xray if the attribute is not 0
-            this.nearestTarget = this.mob.level().getNearestEntity(this.targetClass, this.targetEntitySelector, this.mob, this.mob.getX(), this.mob.getEyeY(), this.mob.getZ(), this.getTargetSearchArea(this.getFollowDistance()));
-            if (this.nearestTarget == null && this.getFollowXRayDistance() > 0d) {
-                this.nearestTarget = this.mob.level().getNearestEntity(this.targetClass, this.targetEntitySelectorXRay.range(this.getFollowXRayDistance()), this.mob, this.mob.getX(), this.mob.getEyeY(), this.mob.getZ(), this.getTargetSearchArea(this.getFollowXRayDistance()));
-            }
+            this.nearestTarget = this.mob.level().getNearestEntity(this.targetClass, this.targetEntitySelectorXRay.range(this.getFollowXRayDistance()), this.mob, this.mob.getX(), this.mob.getEyeY(), this.mob.getZ(), this.getTargetSearchArea(this.getFollowXRayDistance()));
         }
         else {
             //Try to find the nearest player without xray, then try with xray if the attribute is not 0
-            this.nearestTarget = this.mob.level().getNearestPlayer(this.targetEntitySelector, this.mob, this.mob.getX(), this.mob.getEyeY(), this.mob.getZ());
-            if (this.nearestTarget == null && this.getFollowXRayDistance() > 0d) {
-                this.nearestTarget = this.mob.level().getNearestPlayer(this.targetEntitySelectorXRay.range(this.getFollowXRayDistance()), this.mob, this.mob.getX(), this.mob.getEyeY(), this.mob.getZ());
-            }
+            this.nearestTarget = this.mob.level().getNearestPlayer(this.targetEntitySelectorXRay.range(this.getFollowXRayDistance()), this.mob, this.mob.getX(), this.mob.getEyeY(), this.mob.getZ());
         }
     }
 
