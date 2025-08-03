53b3ff9a8fdb69daaf2fca89c754bb8fbdc8d771
Insane96 <delvillano.alberto@gmail.com>
2025-08-03 09:26:18 +0200

    Refactor wolf feature to use separate tags for buffs and passive healing, and update related logic and data files.

--- src/main/java/insane96mcp/enhancedai/modules/pets/Wolves.java
+++ src/main/java/insane96mcp/enhancedai/modules/pets/Wolves.java
@@ -7,7 +7,9 @@ import insane96mcp.insanelib.base.LoadFeature;
 import insane96mcp.insanelib.base.Module;
 import insane96mcp.insanelib.base.config.Config;
 import insane96mcp.insanelib.util.MCUtils;
+import insane96mcp.insanelib.util.ModNBTData;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
@@ -19,10 +21,11 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import java.util.UUID;
 
-@LoadFeature(module = Modules.Ids.PETS, description = "Use the enhancedai:change_wolves entity type tag to add more wolves.")
+@LoadFeature(module = Modules.Ids.PETS, description = "Use the enhancedai:wolves/buffs and enhancedai:wolves/passive_healing entity type tags to add more wolves.")
 public class Wolves extends Feature {
-    public static final TagKey<EntityType<?>> CHANGE_WOLVES = TagKey.create(Registries.ENTITY_TYPE, EnhancedAI.location("change_wolves"));
-    private static final String ON_SPAWN_PROCESSED = EnhancedAI.RESOURCE_PREFIX + "wolves_on_spawn_processed";
+    public static final TagKey<EntityType<?>> BUFFS = TagKey.create(Registries.ENTITY_TYPE, EnhancedAI.location("wolves/buffs"));
+    public static final TagKey<EntityType<?>> PASSIVE_HEALING = TagKey.create(Registries.ENTITY_TYPE, EnhancedAI.location("wolves/passive_healing"));
+    private static ResourceLocation BUFFS_PROCESSED;
     @Config
     public static Boolean doubleHpAndDamage = true;
     @Config
@@ -30,8 +33,9 @@ public class Wolves extends Feature {
     @Config(description = "Wolves will slowly heal like horses. This is 1 in x chance to heal 1 hp each tick.")
     public static Integer passiveHealSpeed = 900;
 
-    public Wolves(Module module, boolean enabledByDefault, boolean canBeDisabled) {
-        super(module, enabledByDefault, canBeDisabled);
+    public void init(Module module, boolean enabledByDefault, boolean canBeDisabled) {
+        super.init(module, enabledByDefault, canBeDisabled);
+        BUFFS_PROCESSED = this.createDataKey("buffs_processed");
     }
 
     @SubscribeEvent
@@ -39,8 +43,8 @@ public class Wolves extends Feature {
         if (!this.isEnabled()
                 || !doubleHpAndDamage && !bonusMovementSpeed
                 || !(event.getEntity() instanceof Wolf wolf)
-                || !wolf.getType().is(CHANGE_WOLVES)
-                || wolf.getPersistentData().contains(ON_SPAWN_PROCESSED))
+                || !wolf.getType().is(BUFFS)
+                || ModNBTData.get(wolf, BUFFS_PROCESSED, Boolean.class))
             return;
 
         if (bonusMovementSpeed)
@@ -49,6 +53,7 @@ public class Wolves extends Feature {
             MCUtils.applyModifier(wolf, Attributes.MAX_HEALTH, UUID.fromString("f9353c93-25a5-42f4-a80e-4f4834b12e77"), "More HP for Wolves", 1d, AttributeModifier.Operation.MULTIPLY_BASE, true);
             MCUtils.applyModifier(wolf, Attributes.ATTACK_DAMAGE, UUID.fromString("e5e5bb8d-3eef-4e92-8897-909acdd4be61"), "More Damage for Wolves", 1d, AttributeModifier.Operation.MULTIPLY_BASE, true);
         }
+        ModNBTData.put(wolf, BUFFS_PROCESSED, true);
     }
 
     @SubscribeEvent
@@ -56,7 +61,7 @@ public class Wolves extends Feature {
         if (!this.isEnabled()
                 || passiveHealSpeed == 0
                 || !(event.getEntity() instanceof Wolf wolf)
-                || !wolf.getType().is(CHANGE_WOLVES))
+                || !wolf.getType().is(PASSIVE_HEALING))
             return;
 
         if (wolf.getHealth() < wolf.getMaxHealth() && wolf.getRandom().nextInt(passiveHealSpeed) == 0)
