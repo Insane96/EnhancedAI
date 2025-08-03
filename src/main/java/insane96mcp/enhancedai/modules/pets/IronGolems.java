59b4a6ef7a24d0f1e7e2b4b7b82e2a82078e875f
Insane96 <delvillano.alberto@gmail.com>
2025-08-03 09:29:09 +0200

    Refactor Iron Golem feature to use separate tags for innate resistance and faster fire ticks; update related logic and data files.

--- src/main/java/insane96mcp/enhancedai/modules/pets/IronGolems.java
+++ src/main/java/insane96mcp/enhancedai/modules/pets/IronGolems.java
@@ -4,7 +4,6 @@ import insane96mcp.enhancedai.EnhancedAI;
 import insane96mcp.enhancedai.modules.Modules;
 import insane96mcp.insanelib.base.Feature;
 import insane96mcp.insanelib.base.LoadFeature;
-import insane96mcp.insanelib.base.Module;
 import insane96mcp.insanelib.base.config.Config;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
@@ -14,25 +13,22 @@ import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-@LoadFeature(module = Modules.Ids.PETS, description = "Use the `enhancedai:change_iron_golems` entity type tag to add more iron golems.")
+@LoadFeature(module = Modules.Ids.PETS, description = "Use the `enhancedai:iron_golems/innate_resistance` and `enhancedai:iron_golems/fire_ticks_faster` entity type tag to add more iron golems.")
 public class IronGolems extends Feature {
-    public static final TagKey<EntityType<?>> CHANGE_IRON_GOLEMS = TagKey.create(Registries.ENTITY_TYPE, EnhancedAI.location("change_iron_golems"));
+    public static final TagKey<EntityType<?>> INNATE_RESISTANCE = TagKey.create(Registries.ENTITY_TYPE, EnhancedAI.location("iron_golems/innate_resistance"));
+    public static final TagKey<EntityType<?>> FIRE_TICKS_FASTER = TagKey.create(Registries.ENTITY_TYPE, EnhancedAI.location("iron_golems/fire_ticks_faster"));
+
     @Config(min = 0d, max = 1d, description = "Resistance to damage taken by Iron Golems")
     public static Double innateResistance = 0.4d;
-
     @Config
     public static Boolean fireTicksFaster = true;
-
-    public IronGolems(Module module, boolean enabledByDefault, boolean canBeDisabled) {
-        super(module, enabledByDefault, canBeDisabled);
-    }
 
     @SubscribeEvent
     public void onHurt(LivingHurtEvent event) {
         if (!this.isEnabled()
                 || innateResistance <= 0d
                 || !(event.getEntity() instanceof IronGolem ironGolem)
-                || !ironGolem.getType().is(CHANGE_IRON_GOLEMS))
+                || !ironGolem.getType().is(INNATE_RESISTANCE))
             return;
 
         event.setAmount(event.getAmount() * innateResistance.floatValue());
@@ -43,7 +39,7 @@ public class IronGolems extends Feature {
         if (!this.isEnabled()
                 || !fireTicksFaster
                 || !(event.getEntity() instanceof IronGolem ironGolem)
-                || !ironGolem.getType().is(CHANGE_IRON_GOLEMS))
+                || !ironGolem.getType().is(FIRE_TICKS_FASTER))
             return;
 
         ironGolem.setRemainingFireTicks(Math.max(0, ironGolem.getRemainingFireTicks() - 3));
