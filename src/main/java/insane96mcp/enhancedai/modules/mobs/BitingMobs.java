546eaf411dad4da15994398244464396f5a7e4c6
Alberto Del Villano <delvillano.alberto@gmail.com>
2025-08-16 17:41:08 +0200

    Biting Mobs ported

--- src/main/java/insane96mcp/enhancedai/modules/mobs/BitingMobs.java
+++ src/main/java/insane96mcp/enhancedai/modules/mobs/BitingMobs.java
@@ -1,6 +1,7 @@
 package insane96mcp.enhancedai.modules.mobs;
 
 import insane96mcp.enhancedai.EnhancedAI;
+import insane96mcp.enhancedai.data.EAIData;
 import insane96mcp.enhancedai.modules.Modules;
 import insane96mcp.insanelib.base.Feature;
 import insane96mcp.insanelib.base.LoadFeature;
@@ -17,14 +18,15 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-@LoadFeature(module = Modules.Ids.MOBS, description = "Mobs can bite if are attacked with non-weapons. Only mobs in the enhancedai:can_bite entity type tag can bite.")
+@LoadFeature(module = Modules.Ids.MOBS, description = "Mobs can bite if are attacked with non-weapons. Only mobs in the enhancedai:biting_mobs/can_bite entity type tag can bite. Entity types in `biting_mobs/unaffected_by_bite` tag are unaffected by bites. Damage types in `biting_mobs/doesnt_trigger_bite` tag don't trigger biting mobs.")
 public class BitingMobs extends Feature {
-	public static final TagKey<EntityType<?>> CAN_BITE = TagKey.create(Registries.ENTITY_TYPE, EnhancedAI.location("can_bite"));
-	public static final TagKey<EntityType<?>> UNAFFECTED_BY_BITE = TagKey.create(Registries.ENTITY_TYPE, EnhancedAI.location("unaffected_by_biting_mobs"));
-	public static final TagKey<DamageType> DOESNT_TRIGGER_BITE = TagKey.create(Registries.DAMAGE_TYPE, EnhancedAI.location("doesnt_trigger_bite"));
+	public static final TagKey<EntityType<?>> CAN_BITE = TagKey.create(Registries.ENTITY_TYPE, EnhancedAI.location("biting_mobs/can_bite"));
+	public static final TagKey<EntityType<?>> UNAFFECTED_BY_BITE = TagKey.create(Registries.ENTITY_TYPE, EnhancedAI.location("biting_mobs/unaffected_by_bite"));
+	public static final TagKey<DamageType> DOESNT_TRIGGER_BITE = TagKey.create(Registries.DAMAGE_TYPE, EnhancedAI.location("biting_mobs/doesnt_trigger_bite"));
 	ResourceKey<DamageType> BITE_DAMAGE_TYPE = ResourceKey.create(Registries.DAMAGE_TYPE, EnhancedAI.location("bite"));
 
 	@Config(min = 0d, max = 1d, description = "Chance for a Mob to bite the attacker")
@@ -32,8 +34,14 @@ public class BitingMobs extends Feature {
 	@Config(min = 0d, description = "The damage dealt to the attacker when bit")
 	public static Double damage = 3d;
 
-	public BitingMobs(Module module, boolean enabledByDefault, boolean canBeDisabled) {
-		super(module, enabledByDefault, canBeDisabled);
+	public static EAIData<Double> CHANCE;
+	public static EAIData<Double> DAMAGE;
+
+	@Override
+	public void init(Module module, boolean enabledByDefault, boolean canBeDisabled) {
+		super.init(module, enabledByDefault, canBeDisabled);
+		CHANCE = EAIData.ofDouble(this.createDataKey("chance"));
+		DAMAGE = EAIData.ofDouble(this.createDataKey("damage"));
 	}
 
 	@SubscribeEvent
@@ -49,11 +57,23 @@ public class BitingMobs extends Feature {
 				|| attacker.getMainHandItem().getAttributeModifiers(EquipmentSlot.MAINHAND).containsKey(Attributes.ATTACK_DAMAGE))
 			return;
 
-		if (mob.getRandom().nextDouble() < chance.getByDifficulty(event.getEntity().level())) {
+		if (mob.getRandom().nextDouble() < CHANCE.get(mob)) {
 			DamageSource damageSource = mob.damageSources().source(BITE_DAMAGE_TYPE, mob);
 			if (attacker.isInvulnerableTo(damageSource))
 				return;
-			attacker.hurt(damageSource, damage.floatValue());
+			attacker.hurt(damageSource, DAMAGE.get(mob).floatValue());
 		}
+	}
+
+	@SubscribeEvent
+	public void onJoinLevelEvent(EntityJoinLevelEvent event) {
+		if (!this.isEnabled()
+				|| event.getLevel().isClientSide
+				|| !(event.getEntity() instanceof Mob mob)
+				|| !mob.getType().is(CAN_BITE))
+			return;
+
+		CHANCE.applyIfAbsent(mob, chance.getByDifficulty(mob.level()));
+		DAMAGE.applyIfAbsent(mob, damage);
 	}
 }
