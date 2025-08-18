e18b18fc53352601f678cc906d87b3acfd30bc54
Alberto Del Villano <delvillano.alberto@gmail.com>
2025-08-18 17:50:49 +0200

    Added Sprint in Movement feature

--- src/main/java/insane96mcp/enhancedai/modules/mobs/movement/Movement.java
+++ src/main/java/insane96mcp/enhancedai/modules/mobs/movement/Movement.java
@@ -21,29 +21,39 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import java.util.UUID;
 
-@LoadFeature(module = Modules.Ids.MOBS, description = "Makes mobs be able to move in more ways, like climbing or swim. Only mobs in the enhancedai:movement/can_climb entity type tag are allowed to climb.")
+@LoadFeature(module = Modules.Ids.MOBS, description = "Makes mobs be able to move in more ways, like climbing or swim.")
 public class Movement extends Feature {
-    public static final TagKey<EntityType<?>> ALLOW_CLIMBING = TagKey.create(Registries.ENTITY_TYPE, EnhancedAI.location("movement/can_climb"));
+    public static final TagKey<EntityType<?>> CAN_CLIMB = TagKey.create(Registries.ENTITY_TYPE, EnhancedAI.location("movement/can_climb"));
+    public static final TagKey<EntityType<?>> CAN_SPRINT = TagKey.create(Registries.ENTITY_TYPE, EnhancedAI.location("movement/can_sprint"));
 
     final UUID UUID_SWIM_SPEED_MULTIPLIER = UUID.fromString("6d2cb27e-e5e3-41b9-8108-f74131a90cce");
     final UUID UUID_MOV_SPEED_MULTIPLIER = UUID.fromString("8230810c-c178-4c01-b066-95e831b6defe");
 
-    @Config(min = 0d, max = 1d, description = "Chance for an entity type in `enhancedai:movement/can_climb` to be able to climb.")
+    @Config(min = 0d, max = 1d, description = "Chance for an entity type in `enhancedai:movement/can_climb` to be able to climb. Only entity types in the enhancedai:movement/can_climb tag are allowed to climb.")
     public static Double allowClimbingChance = 1d;
     @Config(min = 0d, max = 8d, description = "Use Mobs Properties Randomness to have more control over this.")
     public static Double bonusMovementSpeed = 0.15d;
 
+	@Config(min = 0, max = 1d, description = "Chance for entity types in the `enhancedai:movement/can_sprint` tag to be able to sprint when close to the target for 5 seconds.")
+	public static Double sprintChance = 0.25d;
+
     @Config(min = 0d, max = 4d, description = "How faster mobs can swim. Setting to 0 will leave the swim speed as vanilla. I recommend using mods like Mobs Properties Randomness to have more control over the attribute.")
     public static Double swimSpeedAdditionMultiplier = 2.5d;
 
-	public static EAIData<Boolean> CAN_CLIMB;
+	public static EAIData<Boolean> CAN_CLIMB_DATA;
+	public static EAIData<Boolean> CAN_SPRINT_DATA;
 
     public void init(Module module, boolean enabledByDefault, boolean canBeDisabled) {
         super.init(module, enabledByDefault, canBeDisabled);
-		CAN_CLIMB = EAIData.ofBool(this.createDataKey("can_climb"), (mob, canClimb) -> {
+		CAN_CLIMB_DATA = EAIData.ofBool(this.createDataKey("can_climb"), (mob, canClimb) -> {
 			GoalHelper.removeGoal(mob.goalSelector, ClimbLaddersGoal.class);
 			if (canClimb)
 				mob.goalSelector.addGoal(3, new ClimbLaddersGoal(mob));
+		});
+		CAN_SPRINT_DATA = EAIData.ofBool(this.createDataKey("can_sprint"), (mob, canSprint) -> {
+			GoalHelper.removeGoal(mob.goalSelector, SprintGoal.class);
+			if (canSprint)
+				mob.goalSelector.addGoal(1, new SprintGoal(mob));
 		});
     }
 
@@ -51,10 +61,11 @@ public class Movement extends Feature {
     public void onMobSpawn(EntityJoinLevelEvent event) {
         if (!this.isEnabled()
                 || !(event.getEntity() instanceof Mob mob)
-                || !mob.getType().is(ALLOW_CLIMBING))
+                || !mob.getType().is(CAN_CLIMB))
             return;
 
-		CAN_CLIMB.applyIfAbsent(mob, mob.getRandom().nextDouble() < allowClimbingChance);
+		CAN_CLIMB_DATA.applyIfAbsent(mob, mob.getRandom().nextDouble() < allowClimbingChance);
+		CAN_SPRINT_DATA.applyIfAbsent(mob, mob.getRandom().nextDouble() < sprintChance);
 
         if (bonusMovementSpeed > 0d)
             MCUtils.applyModifier(mob, Attributes.MOVEMENT_SPEED, UUID_MOV_SPEED_MULTIPLIER, "Enhanced AI Mov Speed Bonus", bonusMovementSpeed, AttributeModifier.Operation.MULTIPLY_BASE, true);
