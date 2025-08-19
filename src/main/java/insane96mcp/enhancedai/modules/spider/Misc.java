e180db01b2db477576b060edaec456758f924d8a
Alberto Del Villano <delvillano.alberto@gmail.com>
2025-08-19 10:44:16 +0200

    Added a de-stuck goal for spiders

--- src/main/java/insane96mcp/enhancedai/modules/spider/Misc.java
+++ src/main/java/insane96mcp/enhancedai/modules/spider/Misc.java
@@ -6,6 +6,7 @@ import insane96mcp.insanelib.base.LoadFeature;
 import insane96mcp.insanelib.base.config.Config;
 import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.entity.monster.Spider;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -14,14 +15,26 @@ public class Misc extends Feature {
 	@Config(min = 0d, max = 1d, description = "Percentage reduction of the fall damage taken by spiders.")
 	public static Double fallDamageReduction = 0.9d;
 
+	@Config(description = "Adds a new AI to spiders that will prevent them from climbing if they were stuck on a wall for more than 2 seconds.")
+	public static Boolean stuckFix = true;
+
 	@SubscribeEvent
 	public void onSpawn(LivingDamageEvent event) {
 		if (!this.isEnabled()
 				|| fallDamageReduction == 0d
 				|| !event.getSource().is(DamageTypeTags.IS_FALL)
-				|| !(event.getEntity() instanceof Spider))
+				|| !(event.getEntity() instanceof Spider spider))
 			return;
 
 		event.setAmount((float) (event.getAmount() * (1d - fallDamageReduction)));
+	}
+
+	@SubscribeEvent
+	public void onSpawn(EntityJoinLevelEvent event) {
+		if (!this.isEnabled()
+				|| !(event.getEntity() instanceof Spider spider)
+				|| !stuckFix)
+			return;
+		spider.goalSelector.addGoal(0, new DestuckWallGoal(spider));
 	}
 }
