dedabeea882b1f1280d6b43306bcaf839c14fcbc
Insane96 <delvillano.alberto@gmail.com>
2025-09-25 22:38:59 +0200

    Fixed (again?) performance issues with animals fleeing

--- src/main/java/insane96mcp/enhancedai/ai/EAAvoidEntityGoal.java
+++ src/main/java/insane96mcp/enhancedai/ai/EAAvoidEntityGoal.java
@@ -39,8 +39,8 @@ public class EAAvoidEntityGoal<T extends LivingEntity> extends Goal {
 		this.goalOwner = entityIn;
 		this.classToAvoid = avoidClass;
 		this.avoidTargetSelector = targetPredicate;
-		this.avoidDistance = avoidDistance * avoidDistance;
-		this.avoidDistanceNear = avoidDistanceNear * avoidDistanceNear;
+		this.avoidDistance = avoidDistance;
+		this.avoidDistanceNear = avoidDistanceNear;
 		this.nearSpeed = nearSpeedIn;
 		this.farSpeed = farSpeedIn;
 		this.predicateOnAvoidEntity = p_i48859_9_;
