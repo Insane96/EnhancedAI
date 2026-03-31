689f3007028384e31d8c543a2f196bca8141c4de
GulpieEXE <jaron.schachner@gmail.com>
2026-03-30 21:35:50 -0400

    Update MinerMobs.java

--- src/main/java/insane96mcp/enhancedai/modules/mobs/miner/MinerMobs.java
+++ src/main/java/insane96mcp/enhancedai/modules/mobs/miner/MinerMobs.java
@@ -65,13 +65,15 @@ public class MinerMobs extends Feature {
 	@Config(description = "Mobs with Miner AI will spawn with a Stone Pickaxe that never drops.")
 	public static Boolean equipStonePick = true;
 	@Config(min = 0, max = 1200, description = "Time in ticks for a mined block to respawn. Set to 0 for no respawn. (20 ticks = 1 second)")
-	public static Integer blockRespawnTime = 0;
+	public static int blockRespawnTime = 0;
 	@Config(description = "If true, block respawn time will scale based on the block's hardness.")
-	public static Boolean scaleRespawnByHardness = false;
+	public static boolean scaleRespawnByHardness = false;
 	@Config(min = 0, max = 1200, description = "Base respawn time in ticks for a block (used if scaling by hardness is enabled).")
-	public static Integer baseRespawnTime = 200;
+	public static int baseRespawnTime = 200;
 	@Config(min = 0d, max = 100d, description = "Multiplier applied to the block's hardness when calculating respawn time (used if scaling by hardness is enabled).")
-	public static Double hardnessRespawnMultiplier = 100d;
+	public static double hardnessRespawnMultiplier = 100d;
+    @Config(description = "If true, when a respawning block respawns on top of an obstructing block, the obstructing block will drop as if mined with a pickaxe (by default the obstructing block drops itself).")
+    public static boolean usePickaxeDrop = false;
 
 	public static EAIData<Boolean> MINER;
 	public static EAIDataEnum<ToolRequirement> TOOL_REQUIREMENT;
@@ -79,10 +81,7 @@ public class MinerMobs extends Feature {
 	public static EAIData<Integer> MAX_TARGET_DISTANCE;
 	public static EAIData<Double> TIME_TO_BREAK_MULTIPLIER;
 	public static EAIDataList<String> DIMENSION_WHITELIST;
-	public static EAIData<Integer> BLOCK_RESPAWN_TIME;
-	public static EAIData<Boolean> SCALE_RESPAWN_BY_HARDNESS;
-	public static EAIData<Integer> BASE_RESPAWN_TIME;
-	public static EAIData<Double> HARDNESS_RESPAWN_MULTIPLIER;
+
 
 	public void init(Module module, boolean enabledByDefault, boolean canBeDisabled) {
 		super.init(module, enabledByDefault, canBeDisabled);
@@ -96,10 +95,6 @@ public class MinerMobs extends Feature {
 		MAX_TARGET_DISTANCE = EAIData.ofInt(this.createDataKey("max_target_distance"));
 		TIME_TO_BREAK_MULTIPLIER = EAIData.ofDouble(this.createDataKey("time_to_break_multiplier"));
 		DIMENSION_WHITELIST = EAIDataList.of(this.createDataKey("dimension_whitelist"), String.class);
-		BLOCK_RESPAWN_TIME = EAIData.ofInt(this.createDataKey("block_respawn_time"));
-		SCALE_RESPAWN_BY_HARDNESS = EAIData.ofBool(this.createDataKey("scale_respawn_by_hardness"));
-		BASE_RESPAWN_TIME = EAIData.ofInt(this.createDataKey("base_respawn_time"));
-		HARDNESS_RESPAWN_MULTIPLIER = EAIData.ofDouble(this.createDataKey("hardness_respawn_multiplier"));
 		net.minecraftforge.common.MinecraftForge.EVENT_BUS.register(MineTowardsTargetGoal.class);
 	}
 
@@ -114,7 +109,7 @@ public class MinerMobs extends Feature {
 
 	//Lowest priority so other mods can set persistent data
 	@SubscribeEvent(priority = EventPriority.LOWEST)
-	public void onEntityJoinLevel(EntityJoinLevelEvent event) {
+	public void onSpawn(EntityJoinLevelEvent event) {
 		if (!this.isEnabled()
 				|| event.getLevel().isClientSide
 				|| !(event.getEntity() instanceof Mob mob)
@@ -133,11 +128,6 @@ public class MinerMobs extends Feature {
 		MAX_TARGET_DISTANCE.applyIfAbsent(mob, maxTargetDistance);
 		TIME_TO_BREAK_MULTIPLIER.applyIfAbsent(mob, timeToBreakMultiplier);
 		DIMENSION_WHITELIST.applyIfAbsent(mob, dimensionWhitelist);
-		BLOCK_RESPAWN_TIME.applyIfAbsent(mob, blockRespawnTime);
-		SCALE_RESPAWN_BY_HARDNESS.applyIfAbsent(mob, scaleRespawnByHardness);
-		BASE_RESPAWN_TIME.applyIfAbsent(mob, baseRespawnTime);
-		HARDNESS_RESPAWN_MULTIPLIER.applyIfAbsent(mob, hardnessRespawnMultiplier);
-
 	}
 
 	public static boolean isValidDimension(Mob mob) {
@@ -159,83 +149,5 @@ public class MinerMobs extends Feature {
 		if (state.hasBlockEntity()) return true;
 		// Could add a tag whitelist or any other dynamic condition
 		return false;
-	}
-	// --- Merged BlockRespawnHandler logic ---
-	@SubscribeEvent
-	public static void onServerTick(TickEvent.ServerTickEvent event) {
-		if (event.phase != TickEvent.Phase.END)
-			return;
-
-		MinecraftServer server = event.getServer();
-		if (server == null)
-			return;
-
-		for (ServerLevel level : server.getAllLevels()) {
-			BlockRespawnData data = BlockRespawnData.get(level);
-			long time = level.getGameTime();
-			boolean changed = false;
-
-			Iterator<Map.Entry<BlockPos, BlockRespawnData.RespawnEntry>> it = data.getEntries().entrySet().iterator();
-			while (it.hasNext()) {
-				Map.Entry<BlockPos, BlockRespawnData.RespawnEntry> entry = it.next();
-				BlockPos pos = entry.getKey();
-				BlockRespawnData.RespawnEntry info = entry.getValue();
-
-				if (time >= info.time) {
-					try {
-						BlockState existing = level.getBlockState(pos);
-						if (!existing.isAir()) {
-							BlockEntity existingBe = level.getBlockEntity(pos);
-
-							LootParams.Builder lootBuilder = new LootParams.Builder(level)
-									.withParameter(LootContextParams.ORIGIN, pos.getCenter())
-									.withOptionalParameter(LootContextParams.BLOCK_ENTITY, existingBe)
-									.withOptionalParameter(LootContextParams.TOOL, ItemStack.EMPTY);
-
-							for (ItemStack drop : existing.getDrops(lootBuilder)) {
-								level.addFreshEntity(new ItemEntity(level,
-										pos.getX() + 0.5d,
-										pos.getY() + 0.5d,
-										pos.getZ() + 0.5d,
-										drop));
-							}
-
-							existing.spawnAfterBreak(level, pos, ItemStack.EMPTY, false);
-							level.removeBlock(pos, false);
-						}
-
-						AABB box = new AABB(pos);
-						for (Entity e : level.getEntities(null, box)) {
-							e.setPos(e.getX(), e.getY() + 1.0, e.getZ());
-						}
-
-						level.setBlock(pos, info.state, 3);
-
-						if (info.nbt != null) {
-							BlockEntity be = level.getBlockEntity(pos);
-							if (be != null) {
-								be.load(info.nbt);
-								be.setChanged();
-							}
-							else {
-								EnhancedAI.LOGGER.warn("BlockEntity missing at {} when respawning; NBT skipped.", pos);
-							}
-						}
-
-						EnhancedAI.LOGGER.debug("Respawned block {} at {}", info.state.getBlock().getName().getString(), pos);
-
-					}
-					catch (Exception e) {
-						EnhancedAI.LOGGER.warn("Failed to respawn block at {}: {}", pos, e.getMessage());
-					}
-
-					it.remove();
-					changed = true;
-				}
-			}
-
-			if (changed)
-				data.setDirty();
-		}
 	}
 }
