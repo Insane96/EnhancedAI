00a6421c8ca9cf721c596e62840d755840c12b57
Insane96 <delvillano.alberto@gmail.com>
2026-04-27 12:50:45 +0200

    Refactored mixin for clarity and replaced @Redirect with @WrapOperation

--- src/main/java/insane96mcp/enhancedai/mixin/MeleeAttackGoalMixin.java
+++ src/main/java/insane96mcp/enhancedai/mixin/MeleeAttackGoalMixin.java
@@ -2,6 +2,8 @@ package insane96mcp.enhancedai.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import insane96mcp.enhancedai.module.mobs.MeleeAttacking;
 import insane96mcp.insanelib.core.feature.Feature;
 import net.minecraft.world.entity.Entity;
@@ -16,7 +18,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MeleeAttackGoal.class)
 public abstract class MeleeAttackGoalMixin extends Goal {
@@ -24,18 +25,9 @@ public abstract class MeleeAttackGoalMixin extends Goal {
 	@Shadow
 	private double speedModifier;
 
-	@Shadow
-	private Path path;
-
 	@Final
 	@Shadow
 	protected PathfinderMob mob;
-
-	@Shadow private int ticksUntilNextAttack;
-
-	@Shadow protected abstract boolean isTimeToAttack();
-
-	@Shadow protected abstract void resetAttackCooldown();
 
 	@ModifyExpressionValue(method = "canUse", at = @At(value = "CONSTANT", args = "longValue=20"))
 	public long onLastCanUseCheck(long constant) {
@@ -44,37 +36,25 @@ public abstract class MeleeAttackGoalMixin extends Goal {
 		return 0L;
 	}
 
-	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/navigation/PathNavigation;moveTo(Lnet/minecraft/world/entity/Entity;D)Z"))
-	public boolean onMoveTo(PathNavigation pathNavigation, Entity entity, double speedModifier) {
+	@WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/navigation/PathNavigation;moveTo(Lnet/minecraft/world/entity/Entity;D)Z"))
+	public boolean onMoveTo(PathNavigation instance, Entity entity, double speed, Operation<Boolean> original) {
 		Path path = this.mob.getNavigation().createPath(entity, 0);
 		return path != null && this.mob.getNavigation().moveTo(path, speedModifier);
 	}
 
-	/*@Inject(method = "canContinueToUse", at = @At(value = "RETURN", ordinal = 2), cancellable = true)
-	public void onCanContinueToUse(CallbackInfoReturnable<Boolean> cir) {
-		if (!cir.getReturnValue() && this.mob.getTarget() != null) {
-			this.path = this.mob.getNavigation().createPath(this.mob.getTarget(), 0);
-			if (this.path != null) {
-				this.mob.getNavigation().moveTo(this.path, this.speedModifier);
-				cir.setReturnValue(true);
-			}
-		}
-		cir.setReturnValue(true);
-	}*/
-
 	@ModifyExpressionValue(method = "canContinueToUse", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/ai/goal/MeleeAttackGoal;followingTargetEvenIfNotSeen:Z"))
 	public boolean onFollowingTargetEvenIfNotSeen_canContinueToUse(boolean value) {
-		if (Feature.isEnabled(MeleeAttacking.class))
-			return true;
-		return value;
-	}
+        if (!Feature.isEnabled(MeleeAttacking.class))
+            return value;
+        return true;
+    }
 
 	@ModifyExpressionValue(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/ai/goal/MeleeAttackGoal;followingTargetEvenIfNotSeen:Z"))
 	public boolean onFollowingTargetEvenIfNotSeen_tick(boolean value) {
-		if (Feature.isEnabled(MeleeAttacking.class))
-			return true;
-		return value;
-	}
+        if (!Feature.isEnabled(MeleeAttacking.class))
+            return value;
+        return true;
+    }
 
 	@ModifyReturnValue(method = "getAttackInterval", at = @At(value = "RETURN"))
 	public int onGetAttackCooldown(int original) {
